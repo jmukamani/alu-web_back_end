8fdf982e36db6f7e0a67d3373b781a12043ef984
jmukamani <j.mukamani@alustudent.com>
2024-09-04 20:11:36 +0200

    duck typing

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -4,19 +4,19 @@ This module provides a function that returns a multiplier function.
 """
 
 
-from typing import List, Tuple
+from typing import Iterable, Sequence, List, Tuple
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """
-    Return a list of tuples, where each tuple contains a string from the input list
+    Return a list of tuples, where each tuple contains a sequence from the input iterable
     and its corresponding length.
 
     Args:
-        lst (List[str]): A list of strings.
+        lst (Iterable[Sequence]): An iterable of sequences (e.g., strings or lists).
 
     Returns:
-        List[Tuple[str, int]]: A list of tuples, each containing a string from the
-                               input list and its length.
+        List[Tuple[Sequence, int]]: A list of tuples, each containing a sequence from the
+                                     input iterable and its length.
     """
     return [(i, len(i)) for i in lst]
