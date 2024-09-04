5276f891192c95f86eb8cc3f54e30696d0484484
jmukamani <j.mukamani@alustudent.com>
2024-09-04 20:09:23 +0200

    update

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-This module provides a function to convert a floating-point number to its string representation.
+This module provides a function to convert a floating-point number
+    to its string representation.
 """
 
 
--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -6,6 +6,7 @@ This module provides a function that returns a multiplier function.
 
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """
     Return a function that multiplies a float by the given multiplier.
@@ -14,8 +15,9 @@ def make_multiplier(multiplier: float) -> Callable[[float], float]:
         multiplier (float): The multiplier to be applied.
 
     Returns:
-        Callable[[float], float]: A function that takes a float and returns the result
-                                  of multiplying it by the multiplier.
+        Callable[[float], float]: 
+            A function that takes a float and returns the result
+                of multiplying it by the multiplier.
     """
     def multiplier_function(value: float) -> float:
         return value * multiplier
